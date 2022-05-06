9951337d0c799ce722a40ab8708ce68a2d0f6131
Nikita Bushuev <mrbushik1@gmail.com>
2022-05-06 23:14:18 +0300

    menu

---
+++ src/Components/Menu.js
@@ -0,0 +1,26 @@
+import React from "react";
+import styled from "styled-components";
+import dbMenu from './DBMenu';
+import ListItem  from "./ListItem";
+const MenuStyled = styled.main`
+background-color: #ccc;
+margin-top: 80px;
+`;
+const SectionMenu = styled.section`
+padding: 30px
+`;
+const Menu =()=>{
+    return (
+<MenuStyled>
+    <SectionMenu>
+        <h2>Бургеры</h2>
+        <ListItem itemList={dbMenu.burger}/>
+    </SectionMenu>
+    <SectionMenu>
+        <h2>Закуски / Напитки</h2>
+        <ListItem itemList={dbMenu.other}/>
+    </SectionMenu>
+</MenuStyled>
+    )
+}
+export default Menu;
