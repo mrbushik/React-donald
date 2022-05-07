073400d43dc7f253b6729473177f64016690ac17
Nikita Bushuev <mrbushik1@gmail.com>
2022-05-07 19:43:58 +0300

    banner

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import dbMenu from './DBMenu';
 import ListItem  from "./ListItem";
+import { Banner } from './Banner'
 const MenuStyled = styled.main`
 background-color: #ccc;
 margin-top: 80px;
@@ -12,6 +13,7 @@ padding: 30px
 const Menu =()=>{
     return (
 <MenuStyled>
+    <Banner/>
     <SectionMenu>
         <h2>Бургеры</h2>
         <ListItem itemList={dbMenu.burger}/>
