892ca652e2cbc1c999b7876c96fca044464f927d
Nikita Bushuev <mrbushik1@gmail.com>
2022-05-11 18:42:50 +0300

    order item counter

--- src/Components/Order/Order.jsx
+++ src/Components/Order/Order.jsx
@@ -4,6 +4,7 @@ import { ButtonCheckout } from '../Style/ButtonCheckout';
 import { OrderListItem } from './OrderListItem';
 import { totalPriceItems } from '../Functions/secondaryFunction'
 import { formatCurrency } from '../Functions/secondaryFunction'
+// import { calculateTotalItems } from '../Functions/secondaryFunction'
 
 const OrderStyled = styled.section`
   position: fixed;
@@ -49,8 +50,13 @@ const EmptyList = styled.p`
 
 export const Order = ({ orders }) => {
 
+
+
+
   const total = orders.reduce((result, order) =>
     totalPriceItems(order) + result, 0)
+  const totalCounter = orders.reduce((result, order) =>
+    order.count + result, 0)
 
   return (
     <OrderStyled>
@@ -64,7 +70,7 @@ export const Order = ({ orders }) => {
       </OrderContent>
       <Total>
         <span>Итого</span>
-        <span>5</span>
+      <span>{totalCounter}</span>
         <TotalPrice>{formatCurrency(total)}</TotalPrice>
       </Total>
       <ButtonCheckout>Оформить</ButtonCheckout>
