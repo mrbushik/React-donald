1073a2ae5cd71dc1dc61a908627ea2e3ab6b7e5c
Nikita Bushuev <mrbushik1@gmail.com>
2022-05-17 16:01:36 +0300

    get data from db

--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
-import dbMenu from '../DBMenu';
 import { ListItem } from './ListItem';
 import { Banner } from './Banner';
+import { useFetch } from '../Hooks/useFetch';
 
 const MenuStyled = styled.main`
   background-color: #ccc;
@@ -14,16 +14,27 @@ const SectionMenu = styled.section`
   padding: 30px;
 `;
 
-export const Menu = ({ setOpenItem }) => (
-  <MenuStyled>
-    <Banner />
-    <SectionMenu>
-      <h2>Бургеры</h2>
-      <ListItem itemList={dbMenu.burger} setOpenItem={setOpenItem} />
-    </SectionMenu>
-    <SectionMenu>
-      <h2>Закуски / Напитки</h2>
-      <ListItem itemList={dbMenu.other} setOpenItem={setOpenItem} />
-    </SectionMenu>
-  </MenuStyled>
-);
+export const Menu = ({ setOpenItem }) => {
+
+  const res = useFetch();
+const dbMenu = res.response;
+  return (
+    <MenuStyled>
+      <Banner />
+   {dbMenu ?  
+   <>
+          <SectionMenu>
+            <h2>Бургеры</h2>
+            <ListItem itemList={dbMenu.burger} setOpenItem={setOpenItem} />
+          </SectionMenu>
+          <SectionMenu>
+            <h2>Закуски / Напитки</h2>
+            <ListItem itemList={dbMenu.other} setOpenItem={setOpenItem} />
+          </SectionMenu>
+   </>:
+   <div>Загрузка...</div>
+      }
+    </MenuStyled>
+  );
+
+}
